decb4c1216ab1e79bdfedeb404caae97da3e6bca
MukhinKirill <neksus.94@mail.ru>
2019-04-17 21:15:28 +0300

    Done our small cashDesk

---
+++ CrmBl/Model/CashDesk.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CrmBl.Model
+{
+    public class CashDesk // virtual object. Don't will save in BD
+    {
+        CrmContext db = new CrmContext();
+        public int Number { get; set; }
+        public Seller Seller { get; set; }
+        public Queue<Cart> Queue { get; set; }
+        public int MaxQueueLenght { get; set; }
+        public int ExitCustomer { get; set; }//счетчик учета человека в очереди, если больше то покупатель ушел
+        public bool IsModel { get; set; }
+        public CashDesk(int number, Seller seller)
+        {
+            Seller = seller;
+            Number = number;
+            Queue = new Queue<Cart>();
+            IsModel = true;
+        }
+        public void Enqueue(Cart cart)
+        {
+            if(Queue.Count <= MaxQueueLenght)
+            {
+                Queue.Enqueue(cart);
+            }
+            else
+            {
+                ExitCustomer++;
+            }
+        }
+        public decimal Dequeue()
+        {
+            decimal sum = 0;
+            var cart = Queue.Dequeue();
+            if(cart !=null)
+            {
+                var check = new Check()
+                {
+                    SellerId = Seller.SellerId,
+                    Seller = Seller,
+                    CustomerId = cart.Customer.CustomerId,
+                    Customer = cart.Customer,
+                    Created = DateTime.Now
+                };
+                if(!IsModel)
+                {
+                    db.Checks.Add(check);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    check.CheckId = 0;
+                }
+                var sells = new List<Sell>();
+                foreach (Product product in cart)
+                {
+                    if (product.Count > 0)
+                    {
+                        var sell = new Sell()
+                        {
+                            CheckId = check.CheckId,
+                            Check = check,
+                            ProductId = product.ProductId,
+                            Product = product
+                        };
+
+
+                        sells.Add(sell);
+                        if (!IsModel)
+                        {
+                            db.Sells.Add(sell);//добавляем наши продажи в локальную бд
+                        }
+                        product.Count--;//по-хорошем тут должна быть транзакция
+                        sum += product.Price;
+                    }
+                }
+                if (!IsModel)
+                {
+                    db.SaveChanges(); //тепеь кидаем их в рабочую бд
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ CrmBlTests/Model/CashDeskTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrmBl.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model.Tests
+{
+    [TestClass()]
+    public class CashDeskTests
+    {
+        [TestMethod()]
+        public void CashDeskTest()
+        {
+            // arrange
+            var customer1 = new Customer()
+            {
+                Name = "testUser1",
+                CustomerId = 1
+            };
+            var customer2 = new Customer()
+            {
+                Name = "testUser2",
+                CustomerId = 2
+            };
+            var seller = new Seller()
+            {
+                Name = "SellerName",
+                SellerId = 1
+            };
+
+            var product1 = new Product()
+            {
+                ProductId = 1,
+                Name = "pr1",
+                Price = 100,
+                Count = 10
+            };
+            var product2 = new Product()
+            {
+                ProductId = 2,
+                Name = "pr2",
+                Price = 200,
+                Count = 20
+            };
+            var cart1 = new Cart(customer1)
+            {
+                product1,
+                product1,
+                product2
+            };
+            var cart2 = new Cart(customer2)
+            {
+                product1,
+                product2,
+                product2
+            };
+            var cashDesk = new CashDesk(1, seller);
+            cashDesk.MaxQueueLenght = 10;
+            cashDesk.Enqueue(cart1);
+            cashDesk.Enqueue(cart2);
+
+            decimal cart1ExpectedResult = 400;
+            decimal cart2ExpectedResult = 500;
+
+            // act
+            var cart1ActualResult = cashDesk.Dequeue();
+            var cart2ActualResult = cashDesk.Dequeue();
+            // assert
+            Assert.AreEqual(cart1ExpectedResult, cart1ActualResult);
+            Assert.AreEqual(cart2ExpectedResult, cart2ActualResult);
+            Assert.AreEqual(7, product1.Count);
+            Assert.AreEqual(17, product2.Count);
+
+        }
+
+
+    }
+}
